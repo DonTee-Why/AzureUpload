364cc2b0730051ccccd6f69b9834b6a13512c2d4
Timilehin Olusegun <timiddon97@gmail.com>
2024-01-21 20:52:51 +0100

    Fixed namespace issue

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AzureUpload.Models;
-using AzureUpload.Models.FileUploadViewModel;
 using Azure.Storage.Blobs;
 
 namespace AzureUpload.Controllers;
--- Models/FileUploadViewModel.cs
+++ Models/FileUploadViewModel.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace AzureUpload.Models.FileUploadViewModel
+namespace AzureUpload.Models
 {
     public class FileUploadViewModel
     {
